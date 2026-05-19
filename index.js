79a6a3f1e089c1acfac66541e2be162c261da07b
Daniel Romão Arvani <daniromaoarvani@gmail.com>
2026-05-19 16:06:50 -0300

    corrige deploy render

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const client = new Client({
     puppeteer: {
         headless: true,
+        executablePath: '/usr/bin/google-chrome-stable',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
